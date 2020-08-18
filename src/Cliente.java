49543bd85c8f8fa5823290d937c10cd2bac965e4
Anderson Lima <andclima@gmail.com>
2020-08-18 00:21:06 -0300

    v3

---
+++ src/Cliente.java
@@ -0,0 +1,37 @@
+public class Cliente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome;
+    }
+    
+}
