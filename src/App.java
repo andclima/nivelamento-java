5a19e76228bb41e3e2a9e395a3bb0ca781dc2ac4
Anderson Lima <andclima@gmail.com>
2020-08-18 00:09:32 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe um número: ");
+        int valor = scanner.nextInt();
+        if (valor % 2 == 0) {
+            System.out.println("Parabéns, vc digitou um número par!");
+        } else {
+            System.out.println("Opa! Vc digitou um número ímpar!");
+        }
+        scanner.close();
+    }
+}
